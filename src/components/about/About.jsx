6800acf566fcf763783c3c80e255074c04f57db3
Nowfeel Safi <noz@macbook-air-de-nowfeel.home>
2022-06-21 00:53:27 +0200

    about card

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import Me from '../../assets/pas-moi-test.webp';
+import { FaAward } from 'react-icons/fa';
+import { FiUsers } from 'react-icons/fi';
+import { VscFolderLibrary } from 'react-icons/vsc';
+
 const About = () => {
   return (
     <section id='about'>
@@ -9,13 +13,34 @@ const About = () => {
       <div className="container about__container">
         <div className="about__me">
         <div className="about__me-img">
-          <img src={Me} alt="Moi qui sourit" />
+          <img src={Me} alt="About Image" />
         </div>
 
         </div>
 
         <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+            <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>3+ Years Experience</small>
+            </article>
+            <article className='about__card'>
+            <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+            <article className='about__card'>
+            <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>80+ Completed</small>
+            </article>
+          </div>
 
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis itaque vitae quae, aut, odio cum obcaecati dolores velit delectus autem ducimus sapiente nemo, quos officiis! Omnis velit assumenda eius quam!
+          </p>
+          <a href="#contact" className='btn btn-primary'> Let's Talk </a>
         </div>
       </div>
     </section>
