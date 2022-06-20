c2a835052bf9fa8fa76cceea6434716938d1962f
Nowfeel Safi <noz@macbook-air-de-nowfeel.home>
2022-06-20 20:22:57 +0200

    create nav and start about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
-
+import Me from '../../assets/pas-moi-test.webp';
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+        <div className="about__me-img">
+          <img src={Me} alt="Moi qui sourit" />
+        </div>
+
+        </div>
+
+        <div className="about__content">
+
+        </div>
+      </div>
+    </section>
   )
 }
 
