25c3dd226bf891fc5d123a3cda775ac8293c64e6
Nowfeel Safi <noz@macbook-air-de-nowfeel.home>
2022-06-21 18:43:15 +0200

    experience component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
       <div className="container about__container">
         <div className="about__me">
         <div className="about__me-img">
-          <img src={Me} alt="About Image" />
+          <img src={Me} alt="About" />
         </div>
 
         </div>
