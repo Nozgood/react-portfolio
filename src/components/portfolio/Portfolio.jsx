0863a4b425d2f6f9724f7f6740c18c520f1b2368
Nowfeel Safi <noz@macbook-air-de-nowfeel.home>
2022-06-22 14:47:48 +0200

    portfolio component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,57 @@
 import React from 'react'
+import nftImg from '../../assets/portfolio-img-exemple.jpg';
+import faqImg from '../../assets/portfolio-img-exemple-2.jpg';
+import ratingImg from '../../assets/portfolio-img-exemple-3.jpg';
 
+
+const data = [
+  {
+    id: 1,
+    image: nftImg,
+    title: 'NFT Card Component',
+    github: 'https://github.com',
+    demo: 'https://github.com',
+  },
+  {
+    id: 2,
+    image: faqImg,
+    title: 'FAQ Card Component',
+    github: 'https://github.com',
+    demo: 'https://github.com',
+  },
+  {
+    id: 3,
+    image: ratingImg,
+    title: 'Rating Interactive Card Component',
+    github: 'https://github.com',
+    demo: 'https://github.com',
+  },
+]
 const Portfolio = () => {
   return (
-    <section id="portfolio">Portfolio</section>
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+              <article key={id} className="portfolio__item">
+                <div className="portfolio__item-image">
+                  <img src={image} alt= {title} />
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__item-cta">
+                  <a href={github} className='btn'>GitHub</a>
+                  <a href={demo} className='btn btn-primary' target='_blank' rel='noreferrer'>Live Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
