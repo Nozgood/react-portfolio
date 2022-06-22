2fb95c39517dfb455a108d9764ac8bba7fc5a08d
Nowfeel Safi <noz@macbook-air-de-nowfeel.home>
2022-06-22 15:52:51 +0200

    testimonial component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -43,7 +43,7 @@ const Portfolio = () => {
                 </div>
                 <h3>{title}</h3>
                 <div className="portfolio__item-cta">
-                  <a href={github} className='btn'>GitHub</a>
+                  <a href={github} className='btn'  target='_blank' rel='noreferrer'>GitHub</a>
                   <a href={demo} className='btn btn-primary' target='_blank' rel='noreferrer'>Live Demo</a>
                 </div>
               </article>
