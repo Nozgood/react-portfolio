033791e21b5470153dda7498853e648359c4de58
nowfeel <nowfeel.safi@gmail.com>
2024-10-30 07:30:16 +0100

    fix: remove twitter refactor: remove useless hooks (email)

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,26 +1,8 @@
 import React from 'react'
 import { MdOutlineEmail } from 'react-icons/md'
-import { AiOutlineTwitter } from 'react-icons/ai'
 import { AiOutlineLinkedin } from 'react-icons/ai'
-import { useRef } from 'react';
-import emailjs from 'emailjs-com';
 
 const Contact = () => {
-
-  const form = useRef();
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs.sendForm('service_bzvsyen', 'template_4gbezva', form.current, 'SlkHTa8_mxo_C_Nia')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
-      
-      e.target.reset();
-  };
 
   return (
     <section id='contact'>
@@ -32,14 +14,8 @@ const Contact = () => {
           <article className="contact__option">
             <MdOutlineEmail className='contact__option-icon'/>
             <h4>Email</h4>
-            <h5>nowfeel.dev.pro@gmail.com</h5>
+            <h5>nowfeel.safi@gmail.com</h5>
             <a href="mailto:nowfeel.dev.pro@gmail.com" target='_blank' rel='noreferrer'>Send a Message</a>
-          </article>
-          <article className="contact__option">
-            <AiOutlineTwitter className='contact__option-icon'/>
-            <h4>Twitter</h4>
-            <h5>Selecto00</h5>
-            <a href="https://twitter.com/messages/compose" target='_blank' rel='noreferrer'>Send a Message</a>
           </article>
           <article className="contact__option">
             <AiOutlineLinkedin className='contact__option-icon'/>
