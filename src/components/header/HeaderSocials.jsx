c1bbfb3674d6071a199d9340e64eb841b7ab6d84
nowfeel <nowfeel.safi@gmail.com>
2025-02-13 14:21:52 +0100

    fix: remove header social twitter

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import { FaLinkedin } from 'react-icons/fa';
 import { FaGithub } from 'react-icons/fa';
-import { FaTwitter } from 'react-icons/fa';
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
         <a href="https://www.linkedin.com/in/nowfeel-safi-0294531b5/" target="_blank" rel='noopener noreferrer'> <FaLinkedin /></a>
         <a href="https://github.com/nozgood" target="_blank" rel='noopener noreferrer'> <FaGithub /></a>
-        <a href="https://twitter.com/selecto00" target="_blank" rel='noopener noreferrer'> <FaTwitter /></a>
     </div>
   )
 }
