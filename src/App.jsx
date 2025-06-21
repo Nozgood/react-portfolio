3f4e9d8649a2fff56b60a2af96ea4e1d1a148790
noz <nowfeel.safi@gmail.com>
2025-06-21 12:56:13 +0200

    chore: remove portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
 import React from 'react'
-import About from './components/about/About';
-import Contact from './components/contact/Contact';
-import Experience from './components/experience/Experience';
-import Footer from './components/footer/Footer';
-import Header from './components/header/Header'
-import Nav from './components/nav/Nav';
+import NewProject from './components/newproject/NewProject'
 
 const App = () => {
-    return (
-        <>
-            <Header />
-            <Nav />
-            <About />
-            <Experience />
-            <Contact />
-            <Footer />
-        </>
-    )
+	return (
+		<>
+			<NewProject />
+		</>
+	)
 }
 
 export default App
---
+++ src/components/newproject/NewProject.jsx
@@ -0,0 +1,9 @@
+const NewProject = () => {
+	return (
+		<section className="new_project">
+			<h1> Working on a new portfolio ... See you soon </h1>
+		</section>
+	)
+}
+
+export default NewProject
